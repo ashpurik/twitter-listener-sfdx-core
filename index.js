e4c7a9188d766f828d0cda9a07d7877d4964efd9
Anastasia <ashpurik@salesforce.com>
2018-09-17 15:08:00 -0600

    Use Salesforce core libraries

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ global.exit = function exit(code, msg) { console.log(`ERROR: ${msg}`); process.e
 global.missing = function missing(variable) { exit(1, `${variable} environment variable required.`); }
 
 let { twitter, track } = require('./app/twitter');
-let { org, force } = require('./app/salesforce');
+let { getOrg } = require('./app/salesforce');
 
 let tweetStream = twitter.stream('statuses/filter', { track });
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Web disabled'))
+const app = express();
+const port = process.env.PORT || 3000;
+
+const sendPlatformEvent = async (tweet) => {
+    const org = await getOrg();
+    
+    try {
+        let event = await org.getConnection().sobject('Notification__e').create({'Message__c': tweet.text});
+        console.log('Tweet published from', tweet.user.screen_name);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+app.get('/', (req, res) => {
+    sendPlatformEvent({
+        text: "Hello!",
+        user: {
+            screen_name: 'anastasia_dev'
+        }
+    })
+    res.send('Web disabled');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-tweetStream.on('tweet', (tweet) => {
-    if(!org.authenticated) { return; }
-    
-    let event = force.createSObject('Notification__e');
-    event.set('Message__c', tweet.text);
-
-    org.insert({ sobject: event }, (err) => {
-        if(err) {
-            console.error(err);
-            process.exit(1);
-        }
-        else {
-            console.log('Tweet published from', tweet.user.screen_name);
-        }
-    })
+tweetStream.on('tweet', async (tweet) => {
+    sendPlatformEvent(tweet);
 });
